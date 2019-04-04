572dcb6f59b1db28848547a28350e3f52b515201
Jonathan Tomaney <jon.tomaney@gmail.com>
2019-04-04 16:10:53 +0100

    Passed question 9

--- starting_point/specs/friends_spec.rb
+++ starting_point/specs/friends_spec.rb
@@ -74,68 +74,69 @@ class TestFriends < MiniTest::Test
   # 2. For a given person, return their favourite tv show
   # (e.g. the function favourite_tv_show(@person2) should return the string "Baywatch")
   def test_getting_favourite_tv_show
-      result = get_favourite_tv_show(@person4)
-      assert_equal("X-Files", result)
+    result = get_favourite_tv_show(@person4)
+    assert_equal("X-Files", result)
   end
 
   # 3. For a given person, check if they like a particular food
   # (e.g. the function likes_to_eat(@person2, "bread") should return true, likes_to_eat(@person3, "spinach") should return false)
-def test_likes_to_eat_true
-  result = likes_to_eat(@person2, "soup")
-  assert_equal(true, result)
-end
+  def test_likes_to_eat_true
+    result = likes_to_eat(@person2, "soup")
+    assert_equal(true, result)
+  end
 
-def test_likes_to_eat_false
-  result = likes_to_eat(@person2, "nuts")
-  assert_equal(false, result)
-end
+  def test_likes_to_eat_false
+    result = likes_to_eat(@person2, "nuts")
+    assert_equal(false, result)
+  end
 
-def test_likes_to_eat_true_2
-  result = likes_to_eat(@person2, "bread")
-  assert_equal(true, result)
-end
+  def test_likes_to_eat_true_2
+    result = likes_to_eat(@person2, "bread")
+    assert_equal(true, result)
+  end
   # 4. For a given person, add a new name to their list of friends
   # (e.g. the function add_friend(@person2, "Scrappy-Doo") should add Scrappy-Doo to the friends.)
   # (hint: This function should not return anything. After the function call, check for the length of the friends array to test it!)
-def test_add_friend
-  add_friend(@person2, "Scrappy-Doo")
-  assert_equal(2, @person2[:friends].length)
-end
+  def test_add_friend
+    add_friend(@person2, "Scrappy-Doo")
+    assert_equal(2, @person2[:friends].length)
+  end
 
   # 5. For a given person, remove a specific name from their list of friends
   # (hint: Same as above, testing for the length of the array should be sufficient)
-def test_remove_friend
-  remove_friend(@person3, "Shaggy")
-  assert_equal(1, @person3[:friends].length)
-end
+  def test_remove_friend
+    remove_friend(@person3, "Shaggy")
+    assert_equal(1, @person3[:friends].length)
+  end
 
   # 6. Find the total of everyone's money
   # (hint: use the @people array, remember how we checked the total number of eggs yesterday?)
-def test_total_money
-  result = total_money(@people)
-  assert_equal(143, result)
-end
+  def test_total_money
+    result = total_money(@people)
+    assert_equal(143, result)
+  end
 
   # 7. For two given people, allow the first person to loan a given value of money to the other
   # (hint: our function will probably need 3 arguments passed to it... the lender, the lendee, and the amount for this function)
   # (hint2: You should test if both the lender's and the lendee's money have changed, maybe two assertions?)
-def test_loan
-  loan(@person5, @person1, 10)
-  assert_equal(90, @person5[:monies])
-  assert_equal(11, @person1[:monies])
-end
+  def test_loan
+    loan(@person5, @person1, 10)
+    assert_equal(90, @person5[:monies])
+    assert_equal(11, @person1[:monies])
+  end
 
 
   # 8. Find the set of everyone's favourite food joined together
   # (hint: concatenate the favourites/snack arrays together)
-def test_everyone_favourite_foods
-  result = everyone_favourite_foods(@people)
-  assert_equal(["charcuterie", "soup", "bread", "Scooby snacks", "spaghetti", "ratatouille", "spinach"], result)
-end
+  def test_everyone_favourite_foods
+    result = everyone_favourite_foods(@people)
+    assert_equal(["charcuterie", "soup", "bread", "Scooby snacks", "spaghetti", "ratatouille", "spinach"], result)
+  end
 
   # 9. Find people with no friends
   # (hint: return an array, there might be more people in the future with no friends!)
-def test_no_mates
-
-end
+  def test_no_mates
+    result = no_mates(@people)
+    assert_equal([@person5], result)
+  end
 end
